26966414d29c3426995f4f4385c195959db88086
d59495 <andrea.manzi@comune.fi.it>
2015-05-27 13:26:16 +0200

    progetto iniziale

---
+++ Resources/public/js/pannelloamministrazione.js
@@ -0,0 +1,57 @@
+
+function opendialogrisultato() {
+    $("#risultato").dialog({modal: true, maxHeight: 600, width: 800, dialogClass: "no-close",
+        buttons: [
+            {
+                text: "Chiudi",
+                click: function () {
+                    $(this).dialog("close");
+                }
+            }
+        ]});
+}
+
+$(document).keydown(function (e) {
+    // ESCAPE key pressed
+    if (e.keyCode == 27) {
+        e.preventDefault();
+        jQuery('#risultato').dialog('close');
+    }
+    if (e.keyCode == 13) {
+        e.preventDefault();
+        if (currentfunction) {
+            $("#adminpanel" + currentfunction).click();
+            currentfunction = "";
+        }
+
+    }
+
+});
+
+$("#symfonycommand").focusin(function () {
+    currentfunction = "symfonycommand";
+});
+$("#symfonycommand").focusout(function () {
+    //currentfunction = "";
+});
+
+$("#unixcommand").focusin(function () {
+    currentfunction = "unixcommand";
+});
+$("#unixcommand").focusout(function () {
+    //currentfunction = "";
+});
+
+jQuery('body')
+        .bind(
+                'click',
+                function (e) {
+                    if ($('#risultato').is(':visible') &&
+                            jQuery('#risultato').dialog('isOpen')
+                            && !jQuery(e.target).is('.ui-dialog, a')
+                            && !jQuery(e.target).closest('.ui-dialog').length
+                            ) {
+                        jQuery('#risultato').dialog('close');
+                    }
+                }
+        );
