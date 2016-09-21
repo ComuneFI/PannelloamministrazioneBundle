c5df7ced31382b8865bcfd8eccf8cca273963281
d59495 <andrea.manzi@comune.fi.it>
2016-09-21 11:15:19 +0200

    migliorato codice

--- src/Command/generateentitiesCommand.php
+++ src/Command/generateentitiesCommand.php
@@ -15,10 +15,9 @@ use Symfony\Component\Process\Process;
 use Fi\OsBundle\DependencyInjection\OsFunctions;
 use MwbExporter\Model\Table;
 
-class generateentitiesCommand extends ContainerAwareCommand
-{
-    protected function configure()
-    {
+class generateentitiesCommand extends ContainerAwareCommand {
+
+    protected function configure() {
         $this
                 ->setName('pannelloamministrazione:generateentities')
                 ->setDescription('Genera le entities partendo da un modello workbeanch mwb')
@@ -31,19 +30,18 @@ class generateentitiesCommand extends ContainerAwareCommand
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+    protected function execute(InputInterface $input, OutputInterface $output) {
         set_time_limit(0);
         $fs = new Filesystem();
         $finder = new Finder();
-        $rootdir = $this->getContainer()->get('kernel')->getRootDir().'/..';
-        $appdir = $this->getContainer()->get('kernel')->getRootDir();
-        $cachedir = $appdir.DIRECTORY_SEPARATOR.'cache';
-        $logdir = $appdir.DIRECTORY_SEPARATOR.'logs';
-        $tmpdir = $appdir.DIRECTORY_SEPARATOR.'/tmp';
-        $srcdir = $rootdir.DIRECTORY_SEPARATOR.'/src';
-        $webdir = $rootdir.DIRECTORY_SEPARATOR.'/web';
-        $uploaddir = $webdir.DIRECTORY_SEPARATOR.'/uploads';
+        $rootdir = $this->getContainer()->get('kernel')->getRootDir() . '/..';
+        /* $appdir = $this->getContainer()->get('kernel')->getRootDir();
+          $webdir = $rootdir.DIRECTORY_SEPARATOR.'/web';
+          $cachedir = $appdir.DIRECTORY_SEPARATOR.'cache';
+          $logdir = $appdir.DIRECTORY_SEPARATOR.'logs';
+          $tmpdir = $appdir.DIRECTORY_SEPARATOR.'/tmp';
+          $srcdir = $rootdir.DIRECTORY_SEPARATOR.'/src';
+          $uploaddir = $webdir.DIRECTORY_SEPARATOR.'/uploads'; */
         $prjPath = $rootdir;
 
         $bundlename = $input->getArgument('bundlename');
@@ -60,51 +58,48 @@ class generateentitiesCommand extends ContainerAwareCommand
             $schemaupdate = true;
         }
 
-        $fs = new Filesystem();
-        $wbFile = $prjPath.DIRECTORY_SEPARATOR.'doc'.DIRECTORY_SEPARATOR.$mwbfile;
+        $wbFile = $prjPath . DIRECTORY_SEPARATOR . 'doc' . DIRECTORY_SEPARATOR . $mwbfile;
         $bundlePath = $bundlename;
         if (!$fs->exists($wbFile)) {
-            $output->writeln("<error>Nella cartella 'doc' non è presente il file ".$mwbfile.'!');
-            exit;
+            $output->writeln("<error>Nella cartella 'doc' non è presente il file " . $mwbfile . '!');
+            return 1;
         }
 
-        $scriptGenerator = $prjPath.DIRECTORY_SEPARATOR.'bin'.DIRECTORY_SEPARATOR.'mysql-workbench-schema-export';
+        $scriptGenerator = $prjPath . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'mysql-workbench-schema-export';
 
-        $destinationPath = $prjPath.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.$bundlePath.DIRECTORY_SEPARATOR.'Resources'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR;
+        $destinationPath = $prjPath . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $bundlePath . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR;
 
         if (!$fs->exists($scriptGenerator)) {
             $output->writeln('<error>Non è presente il file export.php del bundle SchemaExporterBundle!</error>');
-            exit;
+            return 1;
         }
         if (!$fs->exists($destinationPath)) {
-            $output->writeln("<error>Non esiste la cartella per l'esportazione ".$destinationPath.', controllare il nome del Bundle!</error>');
-            exit;
+            $output->writeln("<error>Non esiste la cartella per l'esportazione " . $destinationPath . ', controllare il nome del Bundle!</error>');
+            return 1;
         }
-        $viewsPath = $prjPath.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.$bundlePath.DIRECTORY_SEPARATOR.'Resources'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR;
-        $entityPath = $prjPath.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.$bundlePath.DIRECTORY_SEPARATOR.'Entity'.DIRECTORY_SEPARATOR;
-        $formPath = $prjPath.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.$bundlePath.DIRECTORY_SEPARATOR.'Form'.DIRECTORY_SEPARATOR;
+        $viewsPath = $prjPath . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $bundlePath . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR;
+        $entityPath = $prjPath . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $bundlePath . DIRECTORY_SEPARATOR . 'Entity' . DIRECTORY_SEPARATOR;
+        $formPath = $prjPath . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $bundlePath . DIRECTORY_SEPARATOR . 'Form' . DIRECTORY_SEPARATOR;
 
         $fs->mkdir($destinationPath);
         $fs->mkdir($entityPath);
         $fs->mkdir($formPath);
         $fs->mkdir($viewsPath);
-        $output->writeln('Creazione entities yml in '.$destinationPath.' da file '.$mwbfile);
+        $output->writeln('Creazione entities yml in ' . $destinationPath . ' da file ' . $mwbfile);
 
-        $destinationPath = $destinationPath.'doctrine'.DIRECTORY_SEPARATOR;
-        $exportJson = $prjPath.'/app/tmp/export.json';
+        $destinationPath = $destinationPath . 'doctrine' . DIRECTORY_SEPARATOR;
+        $exportJson = $prjPath . '/app/tmp/export.json';
 
         if ($fs->exists($exportJson)) {
             $fs->remove($exportJson);
         }
-        $destinationPathEscaped = str_replace('\\', '/', $destinationPath);
-        $destinationPathEscaped = str_replace('/', "\/", $destinationPathEscaped);
-        $bundlePathEscaped = str_replace('/', '\\', $bundlePath);
-        $bundlePathEscaped = str_replace('\\', '\\\\', $bundlePathEscaped);
+        $destinationPathEscaped = str_replace('/', "\/", str_replace('\\', '/', $destinationPath));
+        $bundlePathEscaped = str_replace('\\', '\\\\', str_replace('/', '\\', $bundlePath));
 
-        $str = file_get_contents($prjPath.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'fi/fifreecorebundle/FiTemplate/config/export.json');
-        $str = str_replace('[bundle]', str_replace('/', '', $bundlePathEscaped), $str);
-        $str = str_replace('[dir]', $destinationPathEscaped, $str);
-        file_put_contents($exportJson, $str);
+        $exportjsonfile = file_get_contents($prjPath . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'fi/fifreecorebundle/src/FiTemplate/config/export.json');
+        $bundlejson = str_replace('[bundle]', str_replace('/', '', $bundlePathEscaped), $exportjsonfile);
+        $exportjsonreplaced = str_replace('[dir]', $destinationPathEscaped, $bundlejson);
+        file_put_contents($exportJson, $exportjsonreplaced);
 
         if (OsFunctions::isWindows()) {
             $phpPath = OsFunctions::getPHPExecutableFromPath();
@@ -114,8 +109,8 @@ class generateentitiesCommand extends ContainerAwareCommand
         $pathsrc = $rootdir;
         $sepchr = self::getSeparator();
 
-        $command = 'cd '.substr($pathsrc, 0, -4).$sepchr
-                .$phpPath.' '.$scriptGenerator.' --export=doctrine2-yaml --config='.$exportJson.' '.$wbFile.' '.$destinationPathEscaped;
+        $command = 'cd ' . substr($pathsrc, 0, -4) . $sepchr
+                . $phpPath . ' ' . $scriptGenerator . ' --export=doctrine2-yaml --config=' . $exportJson . ' ' . $wbFile . ' ' . $destinationPathEscaped;
 
         $process = new Process($command);
         $process->setTimeout(60 * 100);
@@ -127,44 +122,27 @@ class generateentitiesCommand extends ContainerAwareCommand
 
         $pathdoctrineyml = $destinationPath;
 
-        //Si converte il nome file tabella.orm.yml se ha undercore
-        $finder->in($pathdoctrineyml)->files()->name('*_*');
-        $table = new Table();
+        $tablecheck = $this->checktables($pathdoctrineyml, $wbFile, $output);
 
-        foreach ($finder as $file) {
-            $oldfilename = $file->getPathName();
-            $newfilename = $pathdoctrineyml.DIRECTORY_SEPARATOR.$table->beautify($file->getFileName());
-            $fs->rename($oldfilename, $newfilename, true);
+        if ($tablecheck < 0) {
+            return -1;
         }
 
-        //Si cercano file con nomi errati
-        $finderwrong = new Finder();
-        $finderwrong->in($pathdoctrineyml)->files()->name('*_*');
-        $wrongfilename = array();
-        if (count($finderwrong) > 0) {
-            foreach ($finderwrong as $file) {
-                $wrongfilename[] = $file->getFileName();
-                $fs->remove($pathdoctrineyml.DIRECTORY_SEPARATOR.$file->getFileName());
-            }
-        }
-        $finderwrongcapitalize = new Finder();
-        $finderwrongcapitalize->in($pathdoctrineyml)->files()->name('*.yml');
-        foreach ($finderwrongcapitalize as $file) {
-            if (!ctype_upper(substr($file->getFileName(), 0, 1))) {
-                $wrongfilename[] = $file->getFileName();
-                $fs->remove($pathdoctrineyml.DIRECTORY_SEPARATOR.$file->getFileName());
-            }
-        }
         if (!$process->isSuccessful()) {
-            $output->writeln('Errore nel comando '.$command.'<error>'.$process->getErrorOutput().'</error> ');
+            $output->writeln('Errore nel comando ' . $command . '<error>' . $process->getErrorOutput() . '</error> ');
         }
+
         $output->writeln('<info>Entities yml create</info>');
 
-        //return new Response('<pre>Eseguito comando: <i style="color: white;">' . $command . '</i><br/>' . str_replace("\n", "<br/>", $process->getOutput()) . "</pre>");
+        $tablecheck = $this->generateentities($bundlename, $emdest, $schemaupdate, $output);
+        if ($tablecheck < 0) {
+            return -1;
+        }
+    }
 
+    private function generateentities($bundlename, $emdest, $schemaupdate, $output) {
         /* GENERATE ENTITIES */
-        //$bundleName = $request->get("bundlename");
-        $output->writeln('Creazione entities class per il bundle '.str_replace('/', '', $bundlename));
+        $output->writeln('Creazione entities class per il bundle ' . str_replace('/', '', $bundlename));
         $application = new Application($this->getContainer()->get('kernel'));
         $application->setAutoExit(false);
         $command = $this->getApplication()->find('doctrine:generate:entities');
@@ -184,14 +162,56 @@ class generateentitiesCommand extends ContainerAwareCommand
 
             $output->writeln('<info>Aggiornamento database completato</info>');
         }
+        return 0;
     }
 
-    public static function getSeparator()
-    {
+    private function checktables($pathdoctrineyml, $wbFile, $output) {
+        $finder = new Finder();
+        $fs = new Filesystem();
+
+        //Si converte il nome file tabella.orm.yml se ha undercore
+        $finder->in($pathdoctrineyml)->files()->name('*_*');
+        $table = new Table();
+
+        foreach ($finder as $file) {
+            $oldfilename = $file->getPathName();
+            $newfilename = $pathdoctrineyml . DIRECTORY_SEPARATOR . $table->beautify($file->getFileName());
+            $fs->rename($oldfilename, $newfilename, true);
+        }
+
+        //Si cercano file con nomi errati
+        $finderwrong = new Finder();
+        $finderwrong->in($pathdoctrineyml)->files()->name('*_*');
+        $wrongfilename = array();
+        if (count($finderwrong) > 0) {
+            foreach ($finderwrong as $file) {
+                $wrongfilename[] = $file->getFileName();
+                $fs->remove($pathdoctrineyml . DIRECTORY_SEPARATOR . $file->getFileName());
+            }
+        }
+        $finderwrongcapitalize = new Finder();
+        $finderwrongcapitalize->in($pathdoctrineyml)->files()->name('*.yml');
+        foreach ($finderwrongcapitalize as $file) {
+            if (!ctype_upper(substr($file->getFileName(), 0, 1))) {
+                $wrongfilename[] = $file->getFileName();
+                $fs->remove($pathdoctrineyml . DIRECTORY_SEPARATOR . $file->getFileName());
+            }
+        }
+
+        if (count($wrongfilename) > 0) {
+            $output->writeln('<error>Ci sono tabelle nel file ' . $wbFile . ' con nomi non consentiti:' . implode(",", $wrongfilename) . '. I nomi tabella devono essere : con la prima lettera maiuscola,underscore ammesso,doppio underscore non ammesso</error>');
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static function getSeparator() {
         if (OsFunctions::isWindows()) {
             return '&';
         } else {
             return ';';
         }
     }
+
 }
