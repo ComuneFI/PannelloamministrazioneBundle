395d59e24bdee1d9065a852aa14040842160fa0a
Andrea Manzi <manzolo@libero.it>
2016-10-01 12:22:18 +0200

    add test

--- src/Tests/Controller/AdminpanelControllerTest.php
+++ src/Tests/Controller/AdminpanelControllerTest.php
@@ -2,23 +2,52 @@
 
 namespace Fi\PannelloAmministrazioneBundle\Tests\Controller;
 
-use Symfony\Component\DomCrawler\Crawler;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Fi\CoreBundle\DependencyInjection\FifreeTest;
+use Behat\Mink\Mink;
+use Behat\Mink\Session;
 
-class AdminpanelControllerTest extends WebTestCase
+class AdminpanelControllerTest extends FifreeTest
 {
-    public function testAdminpanel()
+    public function testAdminpanelHomepage()
     {
-        $this->assertTrue(true);
-        /* parent::__construct();
-          $this->setClassName(get_class());
-          $client = $this->getClientAutorizzato();
-          $client->request('GET', '/adminpanel');
-          $crawler = new Crawler($client->getResponse()->getContent());
+        parent::setUp();
+        $client = $this->getClientAutorizzato();
+        //$url = $client->getContainer()->get('router')->generate('Ffprincipale');
+        $url = $client->getContainer()->get('router')->generate('fi_pannello_amministrazione_homepage'/* , array('parms' => 'value') */);
 
-          $this->assertTrue($client->getResponse()->isSuccessful());
-          $this->assertGreaterThan(0, $crawler->filter('html:contains("Bundle")')->count());
+        $client->request('GET', $url);
+        $this->assertTrue(
+            $client->getResponse()->headers->contains('Content-Type', 'text/html; charset=UTF-8')
+        );
+    }
 
-         */
+    /*
+     * @test
+     */
+
+    public function testAdminpanelWeb()
+    {
+        parent::__construct();
+        $this->setClassName(get_class());
+        $browser = 'firefox';
+        $client = $this->getClientAutorizzato();
+        //$url = $client->getContainer()->get('router')->generate('Ffprincipale');
+        $urlRouting = $client->getContainer()->get('router')->generate('fi_pannello_amministrazione_homepage'/* , array('parms' => 'value') */);
+        $url = 'http://127.0.0.1:8000'.$urlRouting;
+
+        // Choose a Mink driver. More about it in later chapters.
+        $driver = new \Behat\Mink\Driver\Selenium2Driver($browser);
+        $session = new Session($driver);
+        // start the session
+        $session->start();
+        $session->visit($url);
+        $page = $session->getPage();
+        sleep(1);
+        /* Login */
+        $page->fillField('username', 'admin');
+        $page->fillField('password', 'admin');
+        $page->pressButton('_submit');
+
+        //$session->stop();
     }
 }
---
+++ src/Tests/app/AppKernel.php
@@ -0,0 +1,41 @@
+<?php
+
+// Tests/AppKernel.php (you can define it in a subdirectory /Fixtures if you prefer)
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Fi\CoreBundle\FiCoreBundle(),
+            new Fi\PannelloAmministrazioneBundle\FiPannelloAmministrazioneBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
+            new Oneup\FlysystemBundle\OneupFlysystemBundle(),
+        );
+
+        if (in_array($this->getEnvironment(), array('dev', 'test', 'localhost'))) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config.yml');
+    }
+}
