5a7cf55b80664058a183b10b304451489d7e864a
Andrea Manzi <manzolo@libero.it>
2017-01-08 21:39:47 +0100

    refactor

---
+++ src/DependencyInjection/PannelloAmministrazioneUtils.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Fi\PannelloAmministrazioneBundle\DependencyInjection;
+
+use Fi\OsBundle\DependencyInjection\OsFunctions;
+use Symfony\Component\Process\Process;
+use Fi\PannelloAmministrazioneBundle\DependencyInjection\ProjectPath;
+
+class PannelloAmministrazioneUtils
+{
+
+    private $container;
+    private $apppaths;
+
+    public function __construct($container)
+    {
+        $this->container = $container;
+        $this->apppaths = new ProjectPath($container);
+    }
+
+    public function clearcache()
+    {
+        if (OsFunctions::isWindows()) {
+            $phpPath = OsFunctions::getPHPExecutableFromPath();
+        } else {
+            $phpPath = '/usr/bin/php';
+        }
+        $command = $phpPath . ' ' . $this->apppaths->getConsole() . ' cache:clear '
+                . '--env=' . $this->container->get('kernel')->getEnvironment();
+
+        return PannelloAmministrazioneUtils::runCommand($command);
+    }
+
+    public function runCommand($command)
+    {
+        /* @var $process \Symfony\Component\Process\Process */
+        $return = array();
+        $process = new Process($command);
+        $process->setTimeout(60 * 100);
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            $return = array("errcode" => -1, "errmsg" => 'Errore nel comando ' . $command . '<error>' . $process->getErrorOutput() . '</error>');
+        } else {
+            $return = array("errcode" => 0, "errmsg" => $process->getOutput());
+        }
+
+        return $return;
+    }
+}
