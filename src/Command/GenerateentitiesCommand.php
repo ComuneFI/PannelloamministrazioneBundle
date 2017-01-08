b6fee59c2b1548ba9941e81c5a61bef4d6e7627d
Andrea Manzi <manzolo@libero.it>
2017-01-08 19:12:02 +0100

    refactor

--- src/Command/GenerateentitiesCommand.php
+++ src/Command/GenerateentitiesCommand.php
@@ -7,13 +7,11 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Finder\Finder;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 use Fi\OsBundle\DependencyInjection\OsFunctions;
 use Fi\PannelloAmministrazioneBundle\DependencyInjection\ProjectPath;
 use Fi\PannelloAmministrazioneBundle\DependencyInjection\GeneratorHelper;
-use MwbExporter\Model\Table;
 
 class GenerateentitiesCommand extends ContainerAwareCommand
 {
@@ -53,7 +51,7 @@ class GenerateentitiesCommand extends ContainerAwareCommand
         }
 
         $wbFile = $this->apppaths->getDocPath() . DIRECTORY_SEPARATOR . $mwbfile;
-        $checkprerequisiti = $this->checkprerequisiti($bundlename, $mwbfile, $output);
+        $checkprerequisiti = $this->genhelper->checkprerequisiti($bundlename, $mwbfile, $output);
 
         if ($checkprerequisiti < 0) {
             return -1;
@@ -70,7 +68,7 @@ class GenerateentitiesCommand extends ContainerAwareCommand
 
         $this->removeExportJsonFile();
 
-        $tablecheck = $this->checktables($destinationPath, $wbFile, $output);
+        $tablecheck = $this->genhelper->checktables($destinationPath, $wbFile, $output);
 
         if ($tablecheck < 0) {
             return 1;
@@ -87,62 +85,10 @@ class GenerateentitiesCommand extends ContainerAwareCommand
         return 0;
     }
 
-    private function checkprerequisiti($bundlename, $mwbfile, $output)
-    {
-        $fs = new Filesystem();
-
-        $wbFile = $this->apppaths->getDocPath() . DIRECTORY_SEPARATOR . $mwbfile;
-        $bundlePath = $this->apppaths->getSrcPath() . DIRECTORY_SEPARATOR . $bundlename;
-
-        $viewsPath = $bundlePath .
-                DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR;
-        $entityPath = $bundlePath .
-                DIRECTORY_SEPARATOR . 'Entity' . DIRECTORY_SEPARATOR;
-        $formPath = $bundlePath .
-                DIRECTORY_SEPARATOR . 'Form' . DIRECTORY_SEPARATOR;
-
-        $scriptGenerator = $this->getScriptGenerator();
-
-        $destinationPath = $this->genhelper->getDestinationEntityYmlPath($bundlename);
-        $output->writeln('Creazione entities yml in ' . $destinationPath . ' da file ' . $mwbfile);
-        $destinationPath = $destinationPath . 'doctrine' . DIRECTORY_SEPARATOR;
-
-        if (!$fs->exists($bundlePath)) {
-            $output->writeln('<error>Non esiste la cartella del bundle ' . $bundlePath . '</error>');
-
-            return -1;
-        }
-
-        /* Creazione cartelle se non esistono nel bundle per l'esportazione */
-        $fs->mkdir($destinationPath);
-        $fs->mkdir($entityPath);
-        $fs->mkdir($formPath);
-        $fs->mkdir($viewsPath);
-
-        if (!$fs->exists($wbFile)) {
-            $output->writeln("<error>Nella cartella 'doc' non è presente il file " . $mwbfile . '!');
-
-            return -1;
-        }
-
-        if (!$fs->exists($scriptGenerator)) {
-            $output->writeln('<error>Non è presente il comando ' . $scriptGenerator . ' per esportare il modello!</error>');
-
-            return -1;
-        }
-        if (!$fs->exists($destinationPath)) {
-            $output->writeln("<error>Non esiste la cartella per l'esportazione " . $destinationPath . ', controllare il nome del Bundle!</error>');
-
-            return -1;
-        }
-
-        return 0;
-    }
-
     private function getExportJsonCommand($bundlePath, $wbFile)
     {
         $exportJson = $this->getExportJsonFile();
-        $scriptGenerator = $this->getScriptGenerator();
+        $scriptGenerator = $this->genhelper->getScriptGenerator();
         $destinationPathEscaped = str_replace('/', "\/", str_replace('\\', '/', $this->genhelper->getDestinationEntityYmlPath($bundlePath)));
         $bundlePathEscaped = str_replace('\\', '\\\\', str_replace('/', '\\', $bundlePath));
 
@@ -223,31 +169,6 @@ class GenerateentitiesCommand extends ContainerAwareCommand
         return 0;
     }
 
-    private function getScriptGenerator()
-    {
-        $scriptGenerator = "";
-        if (version_compare(\Symfony\Component\HttpKernel\Kernel::VERSION, '3.0') >= 0) {
-            $scriptGenerator = $this->apppaths->getVendorBinPath() . DIRECTORY_SEPARATOR . 'mysql-workbench-schema-export';
-        } else {
-            try {
-                $scriptGenerator = $this->apppaths->getBinPath() . DIRECTORY_SEPARATOR . 'mysql-workbench-schema-export';
-            } catch (\Exception $exc) {
-                //echo $exc->getTraceAsString();
-                if (!file_exists($scriptGenerator)) {
-                    $scriptGenerator = $this->apppaths->getVendorBinPath() . DIRECTORY_SEPARATOR . 'mysql-workbench-schema-export';
-                }
-            }
-        }
-        if (!file_exists($scriptGenerator)) {
-            $scriptGenerator = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR .
-                    'vendor' . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'mysql-workbench-schema-export';
-        }
-        if (!$scriptGenerator) {
-            throw new \Exception("mysql-workbench-schema-export non trovato", -100);
-        }
-        return $scriptGenerator;
-    }
-
     private function generateentities($bundlename, $emdest, $schemaupdate, $output)
     {
         /* GENERATE ENTITIES */
@@ -314,84 +235,5 @@ class GenerateentitiesCommand extends ContainerAwareCommand
         }
 
         return 0;
-    }
-
-//    private function clearCache($output)
-//    {
-//        $output->writeln('<info>Pulizia cache...</info>');
-//        $pathsrc = $this->apppaths->getRootPath();
-//        $sepchr = self::getSeparator();
-//        $console = $this->apppaths->getConsole();
-//        $ccGenerator = $console . ' cache:clear';
-//
-//        if (file_exists($ccGenerator)) {
-//            if (OsFunctions::isWindows()) {
-//                $phpPath = OsFunctions::getPHPExecutableFromPath();
-//            } else {
-//                $phpPath = '/usr/bin/php';
-//            }
-//
-//            $command = 'cd ' . $pathsrc . $sepchr
-//                    . $phpPath . ' ' . $ccGenerator;
-//            /* @var $process \Symfony\Component\Process\Process */
-//            $process = new Process($command);
-//            $process->setTimeout(60 * 100);
-//            $process->run();
-//
-//            if (!$process->isSuccessful()) {
-//                $output->writeln('Errore nel comando ' . $command . '<error>' . $process->getErrorOutput() . '</error> ');
-//            } else {
-//                $output->writeln($process->getOutput());
-//            }
-//        }
-//    }
-
-    private function checktables($destinationPath, $wbFile, $output)
-    {
-        $finder = new Finder();
-        $fs = new Filesystem();
-
-        $pathdoctrineyml = $destinationPath;
-
-        //Si converte il nome file tabella.orm.yml se ha undercore
-        $finder->in($pathdoctrineyml)->files()->name('*_*');
-        $table = new Table();
-
-        foreach ($finder as $file) {
-            $oldfilename = $file->getPathName();
-            $newfilename = $pathdoctrineyml . DIRECTORY_SEPARATOR . $table->beautify($file->getFileName());
-            $fs->rename($oldfilename, $newfilename, true);
-        }
-
-        //Si cercano file con nomi errati
-        $finderwrong = new Finder();
-        $finderwrong->in($pathdoctrineyml)->files()->name('*_*');
-        $wrongfilename = array();
-        if (count($finderwrong) > 0) {
-            foreach ($finderwrong as $file) {
-                $wrongfilename[] = $file->getFileName();
-                $fs->remove($pathdoctrineyml . DIRECTORY_SEPARATOR . $file->getFileName());
-            }
-        }
-        $finderwrongcapitalize = new Finder();
-        $finderwrongcapitalize->in($pathdoctrineyml)->files()->name('*.yml');
-        foreach ($finderwrongcapitalize as $file) {
-            if (!ctype_upper(substr($file->getFileName(), 0, 1))) {
-                $wrongfilename[] = $file->getFileName();
-                $fs->remove($pathdoctrineyml . DIRECTORY_SEPARATOR . $file->getFileName());
-            }
-        }
-
-        if (count($wrongfilename) > 0) {
-            $errout = '<error>Ci sono tabelle nel file ' . $wbFile . ' con nomi non consentiti:' .
-                    implode(',', $wrongfilename) .
-                    '. I nomi tabella devono essere : con la prima lettera maiuscola,underscore ammesso,doppio underscore non ammesso</error>';
-
-            $output->writeln($errout);
-
-            return -1;
-        } else {
-            return 0;
-        }
     }
 }
--- src/DependencyInjection/GeneratorHelper.php
+++ src/DependencyInjection/GeneratorHelper.php
@@ -3,6 +3,8 @@
 namespace Fi\PannelloAmministrazioneBundle\DependencyInjection;
 
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use MwbExporter\Model\Table;
 
 class GeneratorHelper
 {
@@ -21,6 +23,132 @@ class GeneratorHelper
         return $this->apppaths->getSrcPath() . DIRECTORY_SEPARATOR .
                 $bundlePath . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR .
                 'config' . DIRECTORY_SEPARATOR . 'doctrine' . DIRECTORY_SEPARATOR;
+    }
+
+    public function checktables($destinationPath, $wbFile, $output)
+    {
+        $finder = new Finder();
+        $fs = new Filesystem();
+
+        $pathdoctrineyml = $destinationPath;
+
+        //Si converte il nome file tabella.orm.yml se ha undercore
+        $finder->in($pathdoctrineyml)->files()->name('*_*');
+        $table = new Table();
+
+        foreach ($finder as $file) {
+            $oldfilename = $file->getPathName();
+            $newfilename = $pathdoctrineyml . DIRECTORY_SEPARATOR . $table->beautify($file->getFileName());
+            $fs->rename($oldfilename, $newfilename, true);
+        }
+
+        //Si cercano file con nomi errati
+        $finderwrong = new Finder();
+        $finderwrong->in($pathdoctrineyml)->files()->name('*_*');
+        $wrongfilename = array();
+        if (count($finderwrong) > 0) {
+            foreach ($finderwrong as $file) {
+                $wrongfilename[] = $file->getFileName();
+                $fs->remove($pathdoctrineyml . DIRECTORY_SEPARATOR . $file->getFileName());
+            }
+        }
+        $finderwrongcapitalize = new Finder();
+        $finderwrongcapitalize->in($pathdoctrineyml)->files()->name('*.yml');
+        foreach ($finderwrongcapitalize as $file) {
+            if (!ctype_upper(substr($file->getFileName(), 0, 1))) {
+                $wrongfilename[] = $file->getFileName();
+                $fs->remove($pathdoctrineyml . DIRECTORY_SEPARATOR . $file->getFileName());
+            }
+        }
+
+        if (count($wrongfilename) > 0) {
+            $errout = '<error>Ci sono tabelle nel file ' . $wbFile . ' con nomi non consentiti:' .
+                    implode(',', $wrongfilename) .
+                    '. I nomi tabella devono essere : con la prima lettera maiuscola,underscore ammesso,doppio underscore non ammesso</error>';
+
+            $output->writeln($errout);
+
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    public function checkprerequisiti($bundlename, $mwbfile, $output)
+    {
+        $fs = new Filesystem();
+
+        $wbFile = $this->apppaths->getDocPath() . DIRECTORY_SEPARATOR . $mwbfile;
+        $bundlePath = $this->apppaths->getSrcPath() . DIRECTORY_SEPARATOR . $bundlename;
+
+        $viewsPath = $bundlePath .
+                DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR;
+        $entityPath = $bundlePath .
+                DIRECTORY_SEPARATOR . 'Entity' . DIRECTORY_SEPARATOR;
+        $formPath = $bundlePath .
+                DIRECTORY_SEPARATOR . 'Form' . DIRECTORY_SEPARATOR;
+
+        $scriptGenerator = $this->getScriptGenerator();
+
+        $destinationPath = $this->getDestinationEntityYmlPath($bundlename);
+        $output->writeln('Creazione entities yml in ' . $destinationPath . ' da file ' . $mwbfile);
+        $destinationPath = $destinationPath . 'doctrine' . DIRECTORY_SEPARATOR;
+
+        if (!$fs->exists($bundlePath)) {
+            $output->writeln('<error>Non esiste la cartella del bundle ' . $bundlePath . '</error>');
+
+            return -1;
+        }
+
+        /* Creazione cartelle se non esistono nel bundle per l'esportazione */
+        $fs->mkdir($destinationPath);
+        $fs->mkdir($entityPath);
+        $fs->mkdir($formPath);
+        $fs->mkdir($viewsPath);
+
+        if (!$fs->exists($wbFile)) {
+            $output->writeln("<error>Nella cartella 'doc' non è presente il file " . $mwbfile . '!');
+
+            return -1;
+        }
+
+        if (!$fs->exists($scriptGenerator)) {
+            $output->writeln('<error>Non è presente il comando ' . $scriptGenerator . ' per esportare il modello!</error>');
+
+            return -1;
+        }
+        if (!$fs->exists($destinationPath)) {
+            $output->writeln("<error>Non esiste la cartella per l'esportazione " . $destinationPath . ', controllare il nome del Bundle!</error>');
+
+            return -1;
+        }
+
+        return 0;
+    }
+
+    public function getScriptGenerator()
+    {
+        $scriptGenerator = "";
+        if (version_compare(\Symfony\Component\HttpKernel\Kernel::VERSION, '3.0') >= 0) {
+            $scriptGenerator = $this->apppaths->getVendorBinPath() . DIRECTORY_SEPARATOR . 'mysql-workbench-schema-export';
+        } else {
+            try {
+                $scriptGenerator = $this->apppaths->getBinPath() . DIRECTORY_SEPARATOR . 'mysql-workbench-schema-export';
+            } catch (\Exception $exc) {
+                //echo $exc->getTraceAsString();
+                if (!file_exists($scriptGenerator)) {
+                    $scriptGenerator = $this->apppaths->getVendorBinPath() . DIRECTORY_SEPARATOR . 'mysql-workbench-schema-export';
+                }
+            }
+        }
+        if (!file_exists($scriptGenerator)) {
+            $scriptGenerator = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR .
+                    'vendor' . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'mysql-workbench-schema-export';
+        }
+        if (!$scriptGenerator) {
+            throw new \Exception("mysql-workbench-schema-export non trovato", -100);
+        }
+        return $scriptGenerator;
     }
 
     public static function getJsonMwbGenerator()
